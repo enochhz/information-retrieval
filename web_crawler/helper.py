12b4a767f116b0b0d9a40fe4c4f0f3021aba5823
enochhz <walkerhaozheng@gmail.com>
2020-09-26 22:04:43 -0700

    Update the cralwer

--- web_crawler/helper.py
+++ web_crawler/helper.py
@@ -4,21 +4,16 @@ from bs4 import BeautifulSoup
 import requests
 import operator
 
+#  'English': 'en', 'Chinese': 'zh-cn', 'Spanish': 'es'
 languages = ['af', 'ar', 'bg', 'bn', 'ca', 'cs', 'cy', 'da', 'de', 'el', 'en', 'es', 'et', 'fa', 'fi', 'fr', 'gu', 'he',
               'hi', 'hr', 'hu', 'id', 'it', 'ja', 'kn', 'ko', 'lt', 'lv', 'mk', 'ml', 'mr', 'ne', 'nl', 'no', 'pa', 'pl',
               'pt', 'ro', 'ru', 'sk', 'sl', 'so', 'sq', 'sv', 'sw', 'ta', 'te', 'th', 'tl', 'tr', 'uk', 'ur', 'vi', 'zh-cn', 'zh-tw']
-language_map = {
-    'English': 'en',
-    'Chinese': 'zh-cn',
-    'Spanish': 'es'
-}
 language_tracker_pattern = {lan: 0 for lan in languages}
 
 def detect_language(html_content: str):
     language_tracker = language_tracker_pattern.copy()
     soup = BeautifulSoup(html_content, features = 'lxml')
     texts = set(soup.findAll(text = True))
-    print(len(texts))
     for text in texts:
         try: language_tracker[detect(text)] += 1
         except Exception as e: pass
